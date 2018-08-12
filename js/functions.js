47956ede64df9da31eeb063b2827f2355b76c284
Youssef Ait boudad <youssefaitboudad@live.fr>
2018-08-12 22:29:06 +0000

    update portfolio

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,59 @@
-$(window).scroll(function() {
+(function($) {
+    var firstAppear = true;
+    var wScroll;
 
-	var wScroll = $(this).scrollTop();
+    function myNamePosition() {
+        var windowWidth = $(this).width();
+        wScroll = $(this).scrollTop();
+        if (windowWidth > 630) {
+            $('#myName').css({
+                'transform': 'translate(' + (windowWidth / 3) + 'px, ' + wScroll / 2.5 + '%)',
+                'opacity': '1'
+            })
+        } else {
+            $('#myName').css({
+                'transform': 'translate(0px, ' + wScroll / 2.5 + '%)',
+                'opacity': '1'
+            })
+        };
+    }
+    // this function change the position of myName and set animation to featured-work for first apper
+    function whenFirstAppearl() {
+        myNamePosition();
+        if (firstAppear) {
+            if (wScroll > $('.featured-work').offset().top - ($(window).height() / 1.3)) {
+                firstAppear = false;
+                $('.featured-work figure').each(function(i) {
+                    setTimeout(function() {
+                        $('.featured-work figure').eq(i).addClass('is-showing');
+                    }, 150 * (i + 1));
+                });
+            }
+        }
+    }
+    myNamePosition();
 
-	$('header .content').css({
-		'transform' : 'translate(0px, '+ wScroll /2.5 + '%)'
-	});
+    $(window).on('resize scroll', whenFirstAppearl);
+    // scroll to element
+    function elementScroll() {
+        var offset = $('#navbarHeader').height();
+        var element = $($(this).attr('href')).offset().top - offset;
+        $('body').animate({
+            scrollTop: element
+        }, 500);
+    }
+    $('.navbar li a').click(elementScroll);
 
-	if(wScroll > $('.featured-work').offset().top - ($(window).height() / 1.3)) {
-
-		$('.featured-work figure').each(function(i){
-
-			setTimeout(function(){
-				$('.featured-work figure').eq(i).addClass('is-showing');
-			}, 150 * (i+1));
-
-		});
-
-	}
-
-});
+    var collapsed = false;
+    $("#navbar-toggle").click(function() {
+        collapsed ? collapsed = false : collapsed = true;
+    })
+    //to collapse navbar
+    function collapseNavbar(e) {
+        if (collapsed) {
+            collapsed = false;
+            $('#navbar-collapse').collapse('hide');
+        }
+    }
+    $("#content").click(collapseNavbar);
+})(this.jQuery);
